6c94df0d1f27c95f878cef2da4a63dcd8f8bf997
Patrick-van-Halm <pvanhalm00@gmail.com>
2021-04-10 11:21:23 +0200

    Feature ruleset changer (#7)

--- Settings.cs
+++ Settings.cs
@@ -12,6 +12,19 @@ namespace DVJobsRealism
 		[Draw(Label = "Bonus Payment")]
 		public bool bonusPaymentEnabled = true;
 
+		[Draw(Label = "Max generated jobs")]
+		public int jobsCapacity = 30;
+
+		[Draw(Label = "Max pickups/dropoffs amount")]
+		public int maxShuntingStorageTracks = 3;
+
+		[Draw(Label = "Min cars per job (make sure you have the licenses when changing this)")]
+		public int minCarsPerJob = 3;
+
+		[Draw(Label = "Max cars per job")]
+		public int maxCarsPerJob = 20;
+
+
 		override public void Save(UnityModManager.ModEntry entry)
 		{
 			Save<Settings>(this, entry);
